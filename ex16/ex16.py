e5533b092c9da549377310a192e3126389254817
Helena Flausino <helenaflausino@gmail.com>
2025-04-14 11:49:13 -0300

    refactor(ex16): cria funcao para checar senha

--- ex16/ex16.py
+++ ex16/ex16.py
@@ -12,50 +12,35 @@
 # ord() transforma o caractere no valor ASCII correspondente
 # char() transforma um número no caractere ASCII correspondente
 
-pedir_senha = 'Insira senha com no mínimo 12 caracteres, 2 números, 2 letras maiusculas, 2 minusculas e 2 caracteres especiais: ' # deixar em uma mensagem para o caso de querer mudar mensagem
-invalida = 'Senha inválida' # deixar em uma mensagem para o caso de querer mudar mensagem
-strSenha = str(input(pedir_senha)) # apenas para encurtar
+# depois de algumas tentativas achei melhor criar uma função que retorne se válida ou nao, ignorei inputs do usuário para este script
 
-# contadores dos critérios mínimos
-acentuadas = "áàâãéêíóõôúÁÀÂÃÉÊÍÓÕÔÚçÇ" # não constam na ASCII e são caracteres especiais
-intN = 0
-intLet_mai = 0
-intLet_min = 0
-intCarac_esp = 0
-
-#checar se possui espaços vazios
-for i in strSenha:
-   if ord(i) == 32:
-      print(invalida)
-      strSenha = str(input(pedir_senha))
-
-#checar se possui pelo menos 12 caracteres
-if len(strSenha) < 12:
-  print(invalida)
-  strSenha = str(input(pedir_senha))
-
-else:
-  
-  # contando numeros
-  for i in strSenha:
-    if ord(i) >= 48 and ord(i) <= 57: # contando numeros
-        intN += 1
-    elif ord(i) >= 65 and ord(i) <= 90: # contando letras maiusculas
-        intLet_mai += 1
-    elif ord(i) >= 97 and ord(i) <= 122: # contando letras minusculas
-        intLet_min += 1
-    elif i in list(acentuadas): # contando caracteres especiais lista
-       intCarac_esp += 1
-       # 33-47, 58-64, 91-96, 123-126
-    elif (ord(i) >= 33 and ord(i) <= 47) or (ord(i) >= 58 and ord(i) <= 64) or (ord(i) >= 91 and ord(i) <= 96) or (ord(i) >= 123 and ord(i) <= 126): # contando caracteres especiais ASCII
-       intCarac_esp += 1
-     
-  if intN < 2 and intLet_min < 2 and intLet_mai and intCarac_esp < 2: # verificando criterios mínimos
-    print(invalida)
-    strSenha = str(input(pedir_senha))
+def confere_senha(senha):
     
-print(f'numeros: {intN}, maiusculas: {intLet_mai}, minusculas: {intLet_min}, caracteres: {intCarac_esp}') #apenas para checar
-print('Senha válida')
+    acentuadas = "áàâãéêíóõôúÁÀÂÃÉÊÍÓÕÔÚçÇ" # não constam na ASCII e são caracteres especiais
+    int_n = 0
+    int_let_mai = 0
+    int_let_min = 0
+    int_carac_esp = 0
+    int_vazios = 0
 
-# no começo estava pensando em fazer checagens por criterio e retorno por critério, mas a senha só sera válida se atender todos os critérios
-# sendo assim, achei melhor deixar a chegar para o final e fazendo um unico retorno
+    if len(senha) < 12:
+        return print('Senha inválida'), print(f'numeros: {int_n}, maiusculas: {int_let_mai}, minusculas: {int_let_min}, caracteres: {int_carac_esp}')
+    else:
+        for i in senha:
+            if ord(i) == 32: #checar se possui espaços vazios
+                int_vazios += 1
+            elif ord(i) >= 48 and ord(i) <= 57: # contando numeros
+                int_n += 1
+            elif ord(i) >= 65 and ord(i) <= 90: # contando letras maiusculas
+                int_let_mai += 1
+            elif ord(i) >= 97 and ord(i) <= 122: # contando letras minusculas
+                int_let_min += 1
+            elif i in list(acentuadas): # contando caracteres especiais lista
+                int_carac_esp += 1
+            # 33-47, 58-64, 91-96, 123-126
+            elif (ord(i) >= 33 and ord(i) <= 47) or (ord(i) >= 58 and ord(i) <= 64) or (ord(i) >= 91 and ord(i) <= 96) or (ord(i) >= 123 and ord(i) <= 126): # contando caracteres especiais ASCII
+                int_carac_esp += 1
+        if int_n >= 2 and int_let_min >= 2 and int_let_mai >= 2 and int_carac_esp >= 2 and int_vazios == 0:
+            return print('Senha válida'), print(f'numeros: {int_n}, maiusculas: {int_let_mai}, minusculas: {int_let_min}, caracteres: {int_carac_esp}')
+        else: 
+            print('Senha inválida'), print(f'numeros: {int_n}, maiusculas: {int_let_mai}, minusculas: {int_let_min}, caracteres: {int_carac_esp}')
